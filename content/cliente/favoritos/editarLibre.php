8038e7345022e8d2be015bbee0e56a91f614a10d
jonyspiral <gg@spiralshoes.com>
2026-04-28 13:51:12 -0300

    Trace and force MySQL update for free favorite quantities

--- content/cliente/favoritos/editarLibre.php
+++ content/cliente/favoritos/editarLibre.php
@@ -1,12 +1,23 @@
-<?php require_once('../../../premaster.php'); if (Usuario::logueado()->puede('cliente/favoritos/editar/')) { ?>
+﻿<?php require_once('../../../premaster.php'); if (Usuario::logueado()->puede('cliente/favoritos/editar/')) { ?>
 <?php
 
-$idArticulo = $_POST['idArticulo'];
-$idColor = $_POST['idColor'];
-$cantidades = $_POST['cantidades'];
+$idArticulo = isset($_POST['idArticulo']) ? $_POST['idArticulo'] : null;
+$idColor = isset($_POST['idColor']) ? $_POST['idColor'] : null;
+$cantidades = isset($_POST['cantidades']) ? $_POST['cantidades'] : array();
 $idCliente = Usuario::logueado()->cliente->id;
 
+error_log('[editarLibre] POST=' . print_r($_POST, true));
+error_log('[editarLibre] cliente=' . var_export($idCliente, true) . ' articulo=' . var_export($idArticulo, true) . ' color=' . var_export($idColor, true));
+
 try {
+    if ($idArticulo === null || $idArticulo === '' || $idColor === null || $idColor === '') {
+        throw new FactoryExceptionCustomException('Faltan datos del artículo/color para guardar cantidades.');
+    }
+
+    if (!is_array($cantidades)) {
+        $cantidades = array();
+    }
+
     try {
         $favorito = FavoritoCliente::find($idCliente, $idArticulo, $idColor);
     } catch (FactoryExceptionRegistroNoExistente $ex) {
@@ -16,34 +27,49 @@ try {
         $favorito->articulo = $favorito->colorPorArticulo->articulo;
     }
 
+    $set = array();
     for ($i = 1; $i <= 10; $i++) {
-        $aux = Funciones::toInt($cantidades[$i - 1]);
-        $favorito->cantidades[$i] = $aux >= 0 && $aux < 999 ? $aux : 0;
+        $aux = Funciones::toInt(Funciones::keyIsSet($cantidades, $i - 1, 0));
+        $aux = $aux >= 0 && $aux < 999 ? $aux : 0;
+
+        $favorito->cantidades[$i] = $aux;
+        $set[] = 'cant_' . $i . ' = ' . Datos::objectToDB($aux);
     }
 
     $favorito->guardar();
-    // Compatibilidad MySQL KOI1:
-    // Garantiza persistencia de cantidades libres en favoritos_cliente.cant_1 ... cant_10
-    // antes de confirmar el pedido.
-    $set = array();
-    for ($i = 1; $i <= 10; $i++) {
-        $set[] = 'cant_' . $i . ' = ' . Datos::objectToDB(Funciones::toInt(Funciones::keyIsSet($favorito->cantidades, $i, 0)));
-    }
+
     $set[] = 'fecha_ultima_mod = NOW()';
 
-    Datos::EjecutarSQLsinQuery(
+    $sqlUpdate =
         'UPDATE favoritos_cliente SET ' . implode(', ', $set) .
         ' WHERE cod_cliente = ' . Datos::objectToDB($idCliente) .
         ' AND cod_articulo = ' . Datos::objectToDB($idArticulo) .
-        ' AND cod_color_articulo = ' . Datos::objectToDB($idColor)
-    );
+        ' AND cod_color_articulo = ' . Datos::objectToDB($idColor);
 
+    error_log('[editarLibre] SQL=' . $sqlUpdate);
+
+    Datos::EjecutarSQLsinQuery($sqlUpdate);
+
+    $sqlCheck =
+        'SELECT cod_cliente, cod_articulo, cod_color_articulo, ' .
+        'IFNULL(cant_1,0)+IFNULL(cant_2,0)+IFNULL(cant_3,0)+IFNULL(cant_4,0)+IFNULL(cant_5,0)+' .
+        'IFNULL(cant_6,0)+IFNULL(cant_7,0)+IFNULL(cant_8,0)+IFNULL(cant_9,0)+IFNULL(cant_10,0) AS total_pares ' .
+        'FROM favoritos_cliente ' .
+        'WHERE cod_cliente = ' . Datos::objectToDB($idCliente) .
+        ' AND cod_articulo = ' . Datos::objectToDB($idArticulo) .
+        ' AND cod_color_articulo = ' . Datos::objectToDB($idColor);
+
+    $check = Datos::EjecutarSQLItem($sqlCheck);
+    error_log('[editarLibre] CHECK=' . print_r($check, true));
 
     Html::jsonSuccess('El favorito fue modificado correctamente');
+} catch (FactoryExceptionCustomException $ex) {
+    error_log('[editarLibre] ERROR CUSTOM=' . $ex->getMessage());
+    Html::jsonError($ex->getMessage());
 } catch (Exception $ex) {
-    Html::jsonError('Ocurri� un error al intentar modificar el favorito. ' . $ex->getMessage());
+    error_log('[editarLibre] ERROR=' . $ex->getMessage());
+    Html::jsonError('Ocurrió un error al intentar modificar el favorito. ' . $ex->getMessage());
 }
 
 ?>
 <?php } ?>
-
